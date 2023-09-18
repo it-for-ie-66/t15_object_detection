29c1729e26378751db73d1ca0d88b7f26b631343
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-09-18 12:42:55 +0700

    Init no-webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { load_model } from "./model";
-import {
-  vdo_params,
-  handleWebcamError,
-  getVideoConstraints,
-  displayPredictions,
-  getPrediction,
-} from "./utils";
-import Webcam from "react-webcam";
-import "./App.css";
-import { div } from "@tensorflow/tfjs";
-
-const { width, height, isMobile } = vdo_params();
 
 function App() {
   const [model, setModel] = useState<cocoSsd.ObjectDetection>();
-
   const [ready, setReady] = useState(false);
-
   const [previewImage, setPreviewImage] = useState<any>(null);
-
-  const webcamRef = useRef<any>(null);
-  const [error, setError] = useState("");
-  const [switched, setSwitched] = useState(false);
-
   const [predictions, setPredictions] = useState<
     cocoSsd.DetectedObject[] | null
   >([]);
-  const [isPredicting, setIsPredicting] = useState(false);
-  const sub = useRef<any>(null);
 
   useEffect(() => {
     load_model().then((model) => {
@@ -37,37 +16,6 @@ function App() {
       setReady(true);
     });
   }, []);
-
-  const videoConstraints = getVideoConstraints(
-    width,
-    height,
-    isMobile,
-    switched
-  );
-
-  async function singlePrediction() {
-    const predictions = await getPrediction(model, webcamRef);
-    setPredictions(predictions);
-    if (predictions) displayPredictions(predictions, width, height);
-  }
-
-  function continuousPrediction() {
-    setIsPredicting(true);
-    sub.current = setInterval(async () => {
-      const predictions = await getPrediction(model, webcamRef);
-      setPredictions(predictions);
-      if (predictions) displayPredictions(predictions, width, height);
-    }, 1000);
-  }
-
-  function stopPrediction() {
-    setIsPredicting(false);
-    if (sub.current) clearInterval(sub.current);
-  }
-
-  function switchCamera() {
-    setSwitched(!switched);
-  }
 
   const handleSelectImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     // User needs to select an image
@@ -91,7 +39,6 @@ function App() {
       .detect(e.target)
       .then((predictions) => {
         setPredictions(predictions);
-        if (predictions) displayPredictions(predictions, width, height);
       })
       .catch((err) => {
         console.log(err);
@@ -99,13 +46,11 @@ function App() {
   }
 
   if (!ready) return <div>...loading</div>;
-  if (error) return <div>{error}</div>;
 
-  // Testing image detection, no webcam
   return (
     <>
       <>
-        <h1>Image Classifier</h1>
+        <h1>Object Detection</h1>
         <div>
           <input type="file" onChange={handleSelectImage} />
         </div>
@@ -131,59 +76,6 @@ function App() {
           ))}
         </div>
       </>
-    </>
-  );
-
-  // Actual webcam detection
-  return (
-    <>
-      {/* Menu */}
-      <div className="menu">
-        <button onClick={singlePrediction} disabled={isPredicting}>
-          Single
-        </button>
-        <button onClick={continuousPrediction} disabled={isPredicting}>
-          Continuous
-        </button>
-        <button onClick={stopPrediction} disabled={!isPredicting}>
-          Stop Detect
-        </button>
-        <button onClick={switchCamera} disabled={isPredicting}>
-          Rotate Camera
-        </button>
-      </div>
-      {/* Webcam */}
-      <div
-        className="wrapper"
-        style={{ width: `${width}px`, height: `${height}px` }}
-      >
-        <canvas id="canvas" width={width} height={height} className="canvas" />
-        <div className="webcam">
-          <Webcam
-            audio={false}
-            id="img"
-            ref={webcamRef}
-            width={width}
-            height={height}
-            screenshotQuality={1}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-            onUserMediaError={(err) => handleWebcamError(err, setError)}
-          />
-        </div>
-      </div>
-      {/* Predictions */}
-      <div className="prediction-wrapper">
-        {predictions?.map((prediction, index) => {
-          const percentage = Math.round(prediction.score * 100);
-          const text = `${prediction.class} - ${percentage}%`;
-          return (
-            <div key={index} className="prediction">
-              {text}
-            </div>
-          );
-        })}
-      </div>
     </>
   );
 }
